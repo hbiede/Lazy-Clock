b4b909ae2b2a0c38771c4a811292ad1d778afe3b
hbiede <hbiede@gmail.com>
2018-08-01 12:37:03 -0500

    Version 1.0

---
+++ Lazy Clock/NaturalLanguageTime.swift
@@ -0,0 +1,79 @@
+//
+//  NaturalLanguageTime.swift
+//  Natural Language Time
+//
+//  Created by Hundter Biede on 7/26/18.
+//  Copyright © 2018 Hundter Biede. All rights reserved.
+//
+
+import Foundation
+
+open class NaturalLanguageTime {
+
+    /// Creates a new NatTime object to be used to convert a time to a natural langauge output.
+    ///
+    /// Takes an input of the form 'hh-mm' to create a natural language version of the time, ie an input of "16-30"
+    /// would yield "half past 4"
+    public struct NatTime {
+        private let hourConversion = [0: "Midnight", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten", 11: "Eleven", 12: "Noon", 13: "One", 14: "Two", 15: "Three", 16: "Four", 17: "Five", 18: "Six", 19: "Seven", 20: "Eight", 21: "Nine", 22: "Ten", 23: "Eleven"]
+        private var hours:Int?, minutes:Int?
+        private var natLangString:String = "Default Time"
+        public var timeString: String {
+            get {
+                return (hours == nil || minutes == nil) ? "Default Time" : "\(String(describing: hours!))-\(String(describing: minutes!))"
+            }
+            set {
+                let tempArray = newValue.split(separator: "-")
+                hours = Int(tempArray[0]) ?? nil
+                if tempArray.count > 1 {
+                    minutes = Int(tempArray[1]) ?? nil
+                } else {
+                    minutes = nil
+                }
+                if hours != nil && minutes != nil {
+                    natLangString = toNatLang()
+                } else {
+                    natLangString = "Default Time"
+                }
+            }
+        }
+
+        public init(){}
+
+        public func getNatLangString() -> String! {
+            return natLangString
+        }
+
+        func roundByFive(_ inputNumber: Int!) -> Int {
+            return Int(round(Double(inputNumber) / 5)) * 5
+        }
+
+        func toNatLang() -> String {
+            guard var hourString = hourConversion[hours!] else { return "Default Time" }
+            guard var minuteValue = minutes else { return "Default Time" }
+            minuteValue = roundByFive(minuteValue)
+            var returnStatement:String
+
+            switch minuteValue {
+            case 0:
+                if (hours! == 0 || hours! == 12)
+                {
+                    returnStatement = "\(hourString)"
+                } else {
+                    returnStatement = "\(hourString) o'clock"
+                }
+            case 15:
+                returnStatement = "Quarter past \(hourString)"
+            case 30:
+                returnStatement = "Half past \(hourString)"
+            case 45:
+                hourString = (hours! == 23 ? hourConversion[0] : hourConversion[hours! + 1])!
+                returnStatement = "Quarter til \(hourString)"
+            default:
+                returnStatement = "\(minuteValue) past \(hourString)"
+            }
+
+            return returnStatement.capitalized
+        }
+    }
+}
---
+++ Lazy Clock/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  Digital Clock
+//
+//  Created by Hundter Biede on 7/26/18.
+//  Copyright © 2018 Hundter Biede. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    // UIView Object Outlets
+    @IBOutlet weak var timeLbl: UILabel!
+    @IBOutlet var viewBG: UIView!
+
+    /// Dictionary of the UIColors to be used in the rotation of the clock display.
+    let colorRotation = [UIColor.white, UIColor.red, UIColor.blue, UIColor.green, UIColor.black]
+
+    /// ImpactFeedbackGenerator to be used to create haptic feedback.
+    let impactor = UIImpactFeedbackGenerator.init()
+
+    /// UserDefaults used to store settings for the app.
+    let userSettings = UserDefaults.standard
+
+    /// DateFormatter to be used to update the time, either to the form of short or long form time.
+    let dForm: DateFormatter = {
+        let temp = DateFormatter()
+        temp.dateFormat = "hh-mm"
+        return temp
+    }()
+
+    /// Index to use to cycle through colorRotation[] for the updating of the clock textColor. Defaults to 0.
+    var colorRotationIndex:Int = 0
+
+
+    /// Called after the controller's view is loaded into memory.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Loads saved data for the color of the clock
+        colorRotationIndex = userSettings.integer(forKey: "LazyClock-ColorIndex")
+        updateColor(colorRotation[colorRotationIndex])
+
+        // Sets defaults for the timeLbl
+        timeLbl.adjustsFontSizeToFitWidth = true
+        timeLbl.adjustsFontForContentSizeCategory = true
+        print("Lazy Clock Launched Successfully")
+
+        // Starts the clock on updating every second
+        updateTime()
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTime), userInfo: nil, repeats: true)
+    }
+
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+
+    /// Updates time displayed on display. Called from Timer in viewDidLoad().
+    @objc func updateTime() {
+        var natTime = NaturalLanguageTime.NatTime()
+        natTime.timeString = dForm.string(from: Date())
+        timeLbl.text = natTime.getNatLangString()
+    }
+
+
+    /// Updates the color of timeLbl.textColor.
+    ///
+    /// - Parameters:
+    ///   - newColor: A UIColor to change timeLbl.textColor to.
+    func updateColor(_ newColor: UIColor!) {
+        timeLbl.textColor = newColor
+        if (timeLbl.textColor == UIColor.black) {
+            viewBG.backgroundColor = UIColor.white
+        } else {
+            viewBG.backgroundColor = UIColor.black
+        }
+    }
+
+    /// Rotates colors on single finger touch and saves the color to UserDefaults
+    @IBAction func onTouch(_ sender: Any) {
+        if (timeLbl.textColor == colorRotation.last) {
+            colorRotationIndex = 0
+        } else {
+            colorRotationIndex += 1
+        }
+        updateColor(colorRotation[colorRotationIndex])
+        userSettings.set(colorRotationIndex, forKey: "LazyClock-ColorIndex")
+    }
+}
+
